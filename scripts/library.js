1976a890c931464929d9d219f894e476cc20847b
AliTinai <ali-009@windowslive.com>
2021-07-18 14:03:28 +0400

    Added teh ability to remove books from the library

--- scripts/library.js
+++ scripts/library.js
@@ -28,8 +28,61 @@ function submitBook(e){
 
   myLibrary.push(book);
 
-  console.table(myLibrary);
+  //remove the form and display the library
+  document.querySelector('#book-form').remove();
+  displayLibrary();
+}
 
+function displayLibrary(){
+
+  if(document.querySelector('tbody')){
+    document.querySelector('tbody').remove();
+  }
+
+  //All books are part of the table body of #library-table
+  let library = document.createElement('tbody');
+  document.querySelector('#library-table').appendChild(library);
+
+  //Goes through myLibrary Array to display each book
+  for(let i = 0; i < myLibrary.length; i++){
+
+    let book = document.createElement('tr');
+    library.appendChild(book);
+
+    //put each book property in a table cell (td)
+    for(info in myLibrary[i]){
+
+      let bookData = document.createElement('td');
+
+      //A checkbox is used to display whether the book has been read
+      if(info === 'read'){
+        let readCheckBox = document.createElement('input');
+        readCheckBox.setAttribute('type', 'checkbox');
+
+        if(myLibrary[i][info]){
+          readCheckBox.setAttribute('checked', '');
+        }
+        bookData.appendChild(readCheckBox);
+        book.appendChild(bookData);
+        continue;
+      }
+
+      bookData.textContent = myLibrary[i][info];
+      book.appendChild(bookData);
+    }
+
+    //Add a remove button to each book
+    let removeBtnCell = document.createElement('td');
+    let removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove Book';
+    book.appendChild(removeBtnCell);
+    removeBtnCell.appendChild(removeBtn);
+
+    removeBtn.addEventListener('click', (e) => {
+      myLibrary.splice(i,1);
+      displayLibrary();
+    });
+  }
 }
 
 function displayForm(){
